ef803fab3b170b75aaf91aefc1d36251c3e1994f
tmcquinn-stripe <tmcquinn@stripe.com>
2023-09-12 17:47:57 -0400

    link -> card

--- client/src/views/SrCheckoutForm.vue
+++ client/src/views/SrCheckoutForm.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted, Ref, App } from "vue";
-import { Appearance, PaymentIntentResult, Stripe, StripeElements, StripeElementsOptions, StripeElementsOptionsMode, StripePaymentElement, loadStripe } from "@stripe/stripe-js";
+import { Appearance, DefaultValuesOption, PaymentIntentResult, Stripe, StripeElements, StripeElementsOptions, StripeElementsOptionsMode, StripePaymentElement, StripePaymentElementOptions, loadStripe } from "@stripe/stripe-js";
 
 import SrMessages from "./SrMessages.vue";
 
@@ -9,12 +9,13 @@ const messages: Ref = ref([]);
 
 let stripe: Stripe | null;
 let elements: StripeElements | undefined;
-let paymentElement: StripePaymentElement | undefined;
+let paymentElement: StripePaymentElement | undefined | null;
 let elementOptions: StripeElementsOptionsMode | undefined;
 
 let currentPM:string;
 
 onMounted(async () => {
+  isLoading.value = true
   const { publishableKey } = await fetch("/api/config").then((res) => res.json());
   stripe = await loadStripe(publishableKey, {betas: ["elements_saved_payment_methods_beta_1", "elements_spm_sfu_off_session_override_beta_1"]});
 
@@ -28,7 +29,7 @@ onMounted(async () => {
 
   elementOptions = {
     appearance: appearance,
-    setupFutureUsage: null,
+    setupFutureUsage: "off_session",
     mode: 'payment',
     amount: 1099,
     currency: "usd",
@@ -43,11 +44,12 @@ onMounted(async () => {
   }
   messages.value.push(`Client secret returned.`);
 
-  
+let test:DefaultValuesOption
   elements = stripe?.elements(elementOptions);
-  paymentElement = elements?.create('payment');
+  paymentElement = elements?.create('payment', {
+  })
 
-  paymentElement?.mount("#payment-element");
+  paymentElement?.mount(`#${currentPM}-payment-element`);
   isLoading.value = false;
 });
 
@@ -65,20 +67,29 @@ const getCustomerSessionClientSecret = async () => {
 const handleSubmit = async () => {
   isLoading.value = true;
 
-  currentPM = currentPM == "card" ? "klarna" : "card";
+  currentPM = currentPM == "link" ? "card" : "link";
 
   paymentElement?.destroy();
+  paymentElement = null;
 
   elements?.update({
     // @ts-ignore - customerSessionClientSecret not in public docs
     customerSessionClientSecret: currentPM == 'card' ? (await getCustomerSessionClientSecret()) : null,
-    setupFutureUsage: currentPM == 'card' ? 'off_session' : null,
+    //setupFutureUsage: currentPM == 'link' ? 'off_session' : null,
     paymentMethodTypes: [currentPM]
   })
 
 
-  paymentElement = elements?.create('payment');
-  paymentElement?.mount("#payment-element");
+  const local = elements?.create('payment', (currentPM == 'link' ? {
+    defaultValues: {
+      billingDetails: {
+        email: "tmcquinn@stripe.com"
+      }
+    }
+  }: {}));
+  paymentElement = local
+
+  paymentElement?.mount(`#${currentPM}-payment-element`);
   isLoading.value = false;
 }
 </script>
@@ -98,7 +109,8 @@ const handleSubmit = async () => {
       <div v-show="isLoading" id="loader">
         <img src="shiba-inu.png" style="height: 20px"/>
       </div>
-      <div v-show="!isLoading" id="payment-element" />
+      <div v-show="!isLoading && currentPM === 'card'" id="card-payment-element"/>
+      <div v-show="!isLoading && currentPM === 'link'" id="link-payment-element" />
       <sr-messages :messages="messages" />
     </form>
   </main>
